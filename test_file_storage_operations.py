95e35e05f1d31c2ab2ece7c98dd84a15652e85c2
talhaktkkhan <talhaghostriley@gmail.com>
2026-02-05 19:53:01 +0500

    Add a test_file_storage_operations testing module but it's incomplete

--- test_file_storage_operations.py
+++ test_file_storage_operations.py
@@ -1,4 +1,28 @@
+import string
 import pytest
+import os
+from pathlib import Path
+import time
+import datetime
 from api_client import APIClient
 
 client=APIClient()
+filepath = Path("D:\Testing data\test.txt")
+stats=filepath.stat()
+
+ext=Path(r"D:\Testing data\test.txt").suffix
+
+filename=filepath.name
+
+
+Meta_data={
+    "file_name":filepath.name,
+    "file_type":ext,
+    "file_size":stats.st_size,
+    "file_path":
+}
+
+def test_upload():
+    response = client.post("/api/File_Storage_Operations/Upload_File",file_type,'13')
+    assert response.status_code==200
+    assert "success" in response.text.lower()
